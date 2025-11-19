f84b3008688178600260b9d33f90c1c1c648c5d7
Luiz Fernando <macedoovale@gmail.com>
2025-11-19 20:41:58 -0300

    feat: Update app assets and configuration
    This commit updates various application assets, including logos and manifest files, to reflect recent branding changes and improve PWA compatibility. Additionally, it refines the journey form modal by initializing numeric input fields as potentially empty strings to allow for better user input handling and default values.

--- src/components/ui/Jornada360Icon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Este componente representa o ícone da aplicação Jornada360.
-const Jornada360Icon: React.FC<{ className?: string }> = ({ className }) => (
-    <img src="/assets/logo.png" alt="Jornada360 Logo" className={className} />
-);
-
-export default Jornada360Icon;
