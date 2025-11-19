cef7eb4f4c3967b7b664170122994a7ef252d22b
Luiz Fernando <macedoovale@gmail.com>
2025-11-19 14:48:34 -0300

    Update JourneyFormModal.tsx

--- components/JourneyFormModal.tsx
+++ components/JourneyFormModal.tsx
@@ -1,4 +1,4 @@
-'import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useJourneys } from '../contexts/JourneyContext';
 import { useToast } from '../hooks/useToast';
 import { Journey } from '../types';
